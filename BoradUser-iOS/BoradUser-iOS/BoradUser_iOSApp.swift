336bcde54809f82802383fba4a0c750d3773ee87
LGNLogAN <logan061175@gmail.com>
2025-11-25 15:36:30 +0900

    [Init] 프로젝트 파일 초기 세팅

---
+++ BoradUser-iOS/BoradUser-iOS/BoradUser_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoradUser_iOSApp.swift
+//  BoradUser-iOS
+//
+//  Created by 최무빈 on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BoradUser_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
