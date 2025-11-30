fe4e81f07c2860c2a941bf47a9136e827ab9e82a
LGNLogAN <logan061175@gmail.com>
2025-12-01 01:42:11 +0900

    [Feat] Font Extension 추가

---
+++ BoradUser-iOS/BoradUser-iOS/Extension/Ex+ Font.swift
@@ -0,0 +1,208 @@
+//
+//  Ex+ Font.swift
+//  BoradUser-iOS
+//
+//  Created by 최무빈 on 12/1/25.
+//
+
+import SwiftUI
+
+
+extension Font {
+        
+    // MARK: - Bold (11 ~ 50)
+    static let pretendardBold11: Font = .custom("Pretendard-Bold", size: 11)
+    static let pretendardBold12: Font = .custom("Pretendard-Bold", size: 12)
+    static let pretendardBold13: Font = .custom("Pretendard-Bold", size: 13)
+    static let pretendardBold14: Font = .custom("Pretendard-Bold", size: 14)
+    static let pretendardBold15: Font = .custom("Pretendard-Bold", size: 15)
+    static let pretendardBold16: Font = .custom("Pretendard-Bold", size: 16)
+    static let pretendardBold17: Font = .custom("Pretendard-Bold", size: 17)
+    static let pretendardBold18: Font = .custom("Pretendard-Bold", size: 18)
+    static let pretendardBold19: Font = .custom("Pretendard-Bold", size: 19)
+    static let pretendardBold20: Font = .custom("Pretendard-Bold", size: 20)
+    static let pretendardBold21: Font = .custom("Pretendard-Bold", size: 21)
+    static let pretendardBold22: Font = .custom("Pretendard-Bold", size: 22)
+    static let pretendardBold23: Font = .custom("Pretendard-Bold", size: 23)
+    static let pretendardBold24: Font = .custom("Pretendard-Bold", size: 24)
+    static let pretendardBold25: Font = .custom("Pretendard-Bold", size: 25)
+    static let pretendardBold26: Font = .custom("Pretendard-Bold", size: 26)
+    static let pretendardBold27: Font = .custom("Pretendard-Bold", size: 27)
+    static let pretendardBold28: Font = .custom("Pretendard-Bold", size: 28)
+    static let pretendardBold29: Font = .custom("Pretendard-Bold", size: 29)
+    static let pretendardBold30: Font = .custom("Pretendard-Bold", size: 30)
+    static let pretendardBold31: Font = .custom("Pretendard-Bold", size: 31)
+    static let pretendardBold32: Font = .custom("Pretendard-Bold", size: 32)
+    static let pretendardBold33: Font = .custom("Pretendard-Bold", size: 33)
+    static let pretendardBold34: Font = .custom("Pretendard-Bold", size: 34)
+    static let pretendardBold35: Font = .custom("Pretendard-Bold", size: 35)
+    static let pretendardBold36: Font = .custom("Pretendard-Bold", size: 36)
+    static let pretendardBold37: Font = .custom("Pretendard-Bold", size: 37)
+    static let pretendardBold38: Font = .custom("Pretendard-Bold", size: 38)
+    static let pretendardBold39: Font = .custom("Pretendard-Bold", size: 39)
+    static let pretendardBold40: Font = .custom("Pretendard-Bold", size: 40)
+    static let pretendardBold41: Font = .custom("Pretendard-Bold", size: 41)
+    static let pretendardBold42: Font = .custom("Pretendard-Bold", size: 42)
+    static let pretendardBold43: Font = .custom("Pretendard-Bold", size: 43)
+    static let pretendardBold44: Font = .custom("Pretendard-Bold", size: 44)
+    static let pretendardBold45: Font = .custom("Pretendard-Bold", size: 45)
+    static let pretendardBold46: Font = .custom("Pretendard-Bold", size: 46)
+    static let pretendardBold47: Font = .custom("Pretendard-Bold", size: 47)
+    static let pretendardBold48: Font = .custom("Pretendard-Bold", size: 48)
+    static let pretendardBold49: Font = .custom("Pretendard-Bold", size: 49)
+    static let pretendardBold50: Font = .custom("Pretendard-Bold", size: 50)
+    
+    
+    // MARK: - SemiBold (11 ~ 50)
+    static let pretendardSemiBold11: Font = .custom("Pretendard-SemiBold", size: 11)
+    static let pretendardSemiBold12: Font = .custom("Pretendard-SemiBold", size: 12)
+    static let pretendardSemiBold13: Font = .custom("Pretendard-SemiBold", size: 13)
+    static let pretendardSemiBold14: Font = .custom("Pretendard-SemiBold", size: 14)
+    static let pretendardSemiBold15: Font = .custom("Pretendard-SemiBold", size: 15)
+    static let pretendardSemiBold16: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let pretendardSemiBold17: Font = .custom("Pretendard-SemiBold", size: 17)
+    static let pretendardSemiBold18: Font = .custom("Pretendard-SemiBold", size: 18)
+    static let pretendardSemiBold19: Font = .custom("Pretendard-SemiBold", size: 19)
+    static let pretendardSemiBold20: Font = .custom("Pretendard-SemiBold", size: 20)
+    static let pretendardSemiBold21: Font = .custom("Pretendard-SemiBold", size: 21)
+    static let pretendardSemiBold22: Font = .custom("Pretendard-SemiBold", size: 22)
+    static let pretendardSemiBold23: Font = .custom("Pretendard-SemiBold", size: 23)
+    static let pretendardSemiBold24: Font = .custom("Pretendard-SemiBold", size: 24)
+    static let pretendardSemiBold25: Font = .custom("Pretendard-SemiBold", size: 25)
+    static let pretendardSemiBold26: Font = .custom("Pretendard-SemiBold", size: 26)
+    static let pretendardSemiBold27: Font = .custom("Pretendard-SemiBold", size: 27)
+    static let pretendardSemiBold28: Font = .custom("Pretendard-SemiBold", size: 28)
+    static let pretendardSemiBold29: Font = .custom("Pretendard-SemiBold", size: 29)
+    static let pretendardSemiBold30: Font = .custom("Pretendard-SemiBold", size: 30)
+    static let pretendardSemiBold31: Font = .custom("Pretendard-SemiBold", size: 31)
+    static let pretendardSemiBold32: Font = .custom("Pretendard-SemiBold", size: 32)
+    static let pretendardSemiBold33: Font = .custom("Pretendard-SemiBold", size: 33)
+    static let pretendardSemiBold34: Font = .custom("Pretendard-SemiBold", size: 34)
+    static let pretendardSemiBold35: Font = .custom("Pretendard-SemiBold", size: 35)
+    static let pretendardSemiBold36: Font = .custom("Pretendard-SemiBold", size: 36)
+    static let pretendardSemiBold37: Font = .custom("Pretendard-SemiBold", size: 37)
+    static let pretendardSemiBold38: Font = .custom("Pretendard-SemiBold", size: 38)
+    static let pretendardSemiBold39: Font = .custom("Pretendard-SemiBold", size: 39)
+    static let pretendardSemiBold40: Font = .custom("Pretendard-SemiBold", size: 40)
+    static let pretendardSemiBold41: Font = .custom("Pretendard-SemiBold", size: 41)
+    static let pretendardSemiBold42: Font = .custom("Pretendard-SemiBold", size: 42)
+    static let pretendardSemiBold43: Font = .custom("Pretendard-SemiBold", size: 43)
+    static let pretendardSemiBold44: Font = .custom("Pretendard-SemiBold", size: 44)
+    static let pretendardSemiBold45: Font = .custom("Pretendard-SemiBold", size: 45)
+    static let pretendardSemiBold46: Font = .custom("Pretendard-SemiBold", size: 46)
+    static let pretendardSemiBold47: Font = .custom("Pretendard-SemiBold", size: 47)
+    static let pretendardSemiBold48: Font = .custom("Pretendard-SemiBold", size: 48)
+    static let pretendardSemiBold49: Font = .custom("Pretendard-SemiBold", size: 49)
+    static let pretendardSemiBold50: Font = .custom("Pretendard-SemiBold", size: 50)
+    
+    
+    // MARK: - Medium (11 ~ 50)
+    static let pretendardMedium11: Font = .custom("Pretendard-Medium", size: 11)
+    static let pretendardMedium12: Font = .custom("Pretendard-Medium", size: 12)
+    static let pretendardMedium13: Font = .custom("Pretendard-Medium", size: 13)
+    static let pretendardMedium14: Font = .custom("Pretendard-Medium", size: 14)
+    static let pretendardMedium15: Font = .custom("Pretendard-Medium", size: 15)
+    static let pretendardMedium16: Font = .custom("Pretendard-Medium", size: 16)
+    static let pretendardMedium17: Font = .custom("Pretendard-Medium", size: 17)
+    static let pretendardMedium18: Font = .custom("Pretendard-Medium", size: 18)
+    static let pretendardMedium19: Font = .custom("Pretendard-Medium", size: 19)
+    static let pretendardMedium20: Font = .custom("Pretendard-Medium", size: 20)
+    static let pretendardMedium21: Font = .custom("Pretendard-Medium", size: 21)
+    static let pretendardMedium22: Font = .custom("Pretendard-Medium", size: 22)
+    static let pretendardMedium23: Font = .custom("Pretendard-Medium", size: 23)
+    static let pretendardMedium24: Font = .custom("Pretendard-Medium", size: 24)
+    static let pretendardMedium25: Font = .custom("Pretendard-Medium", size: 25)
+    static let pretendardMedium26: Font = .custom("Pretendard-Medium", size: 26)
+    static let pretendardMedium27: Font = .custom("Pretendard-Medium", size: 27)
+    static let pretendardMedium28: Font = .custom("Pretendard-Medium", size: 28)
+    static let pretendardMedium29: Font = .custom("Pretendard-Medium", size: 29)
+    static let pretendardMedium30: Font = .custom("Pretendard-Medium", size: 30)
+    static let pretendardMedium31: Font = .custom("Pretendard-Medium", size: 31)
+    static let pretendardMedium32: Font = .custom("Pretendard-Medium", size: 32)
+    static let pretendardMedium33: Font = .custom("Pretendard-Medium", size: 33)
+    static let pretendardMedium34: Font = .custom("Pretendard-Medium", size: 34)
+    static let pretendardMedium35: Font = .custom("Pretendard-Medium", size: 35)
+    static let pretendardMedium36: Font = .custom("Pretendard-Medium", size: 36)
+    static let pretendardMedium37: Font = .custom("Pretendard-Medium", size: 37)
+    static let pretendardMedium38: Font = .custom("Pretendard-Medium", size: 38)
+    static let pretendardMedium39: Font = .custom("Pretendard-Medium", size: 39)
+    static let pretendardMedium40: Font = .custom("Pretendard-Medium", size: 40)
+    static let pretendardMedium41: Font = .custom("Pretendard-Medium", size: 41)
+    static let pretendardMedium42: Font = .custom("Pretendard-Medium", size: 42)
+    static let pretendardMedium43: Font = .custom("Pretendard-Medium", size: 43)
+    static let pretendardMedium44: Font = .custom("Pretendard-Medium", size: 44)
+    static let pretendardMedium45: Font = .custom("Pretendard-Medium", size: 45)
+    static let pretendardMedium46: Font = .custom("Pretendard-Medium", size: 46)
+    static let pretendardMedium47: Font = .custom("Pretendard-Medium", size: 47)
+    static let pretendardMedium48: Font = .custom("Pretendard-Medium", size: 48)
+    static let pretendardMedium49: Font = .custom("Pretendard-Medium", size: 49)
+    static let pretendardMedium50: Font = .custom("Pretendard-Medium", size: 50)
+    
+    
+    // MARK: - Regular (11 ~ 50)
+    static let pretendardRegular11: Font = .custom("Pretendard-Regular", size: 11)
+    static let pretendardRegular12: Font = .custom("Pretendard-Regular", size: 12)
+    static let pretendardRegular13: Font = .custom("Pretendard-Regular", size: 13)
+    static let pretendardRegular14: Font = .custom("Pretendard-Regular", size: 14)
+    static let pretendardRegular15: Font = .custom("Pretendard-Regular", size: 15)
+    static let pretendardRegular16: Font = .custom("Pretendard-Regular", size: 16)
+    static let pretendardRegular17: Font = .custom("Pretendard-Regular", size: 17)
+    static let pretendardRegular18: Font = .custom("Pretendard-Regular", size: 18)
+    static let pretendardRegular19: Font = .custom("Pretendard-Regular", size: 19)
+    static let pretendardRegular20: Font = .custom("Pretendard-Regular", size: 20)
+    static let pretendardRegular21: Font = .custom("Pretendard-Regular", size: 21)
+    static let pretendardRegular22: Font = .custom("Pretendard-Regular", size: 22)
+    static let pretendardRegular23: Font = .custom("Pretendard-Regular", size: 23)
+    static let pretendardRegular24: Font = .custom("Pretendard-Regular", size: 24)
+    static let pretendardRegular25: Font = .custom("Pretendard-Regular", size: 25)
+    static let pretendardRegular26: Font = .custom("Pretendard-Regular", size: 26)
+    static let pretendardRegular27: Font = .custom("Pretendard-Regular", size: 27)
+    static let pretendardRegular28: Font = .custom("Pretendard-Regular", size: 28)
+    static let pretendardRegular29: Font = .custom("Pretendard-Regular", size: 29)
+    static let pretendardRegular30: Font = .custom("Pretendard-Regular", size: 30)
+    static let pretendardRegular31: Font = .custom("Pretendard-Regular", size: 31)
+    static let pretendardRegular32: Font = .custom("Pretendard-Regular", size: 32)
+    static let pretendardRegular33: Font = .custom("Pretendard-Regular", size: 33)
+    static let pretendardRegular34: Font = .custom("Pretendard-Regular", size: 34)
+    static let pretendardRegular35: Font = .custom("Pretendard-Regular", size: 35)
+    static let pretendardRegular36: Font = .custom("Pretendard-Regular", size: 36)
+    static let pretendardRegular37: Font = .custom("Pretendard-Regular", size: 37)
+    static let pretendardRegular38: Font = .custom("Pretendard-Regular", size: 38)
+    static let pretendardRegular39: Font = .custom("Pretendard-Regular", size: 39)
+    static let pretendardRegular40: Font = .custom("Pretendard-Regular", size: 40)
+    static let pretendardRegular41: Font = .custom("Pretendard-Regular", size: 41)
+    static let pretendardRegular42: Font = .custom("Pretendard-Regular", size: 42)
+    static let pretendardRegular43: Font = .custom("Pretendard-Regular", size: 43)
+    static let pretendardRegular44: Font = .custom("Pretendard-Regular", size: 44)
+    static let pretendardRegular45: Font = .custom("Pretendard-Regular", size: 45)
+    static let pretendardRegular46: Font = .custom("Pretendard-Regular", size: 46)
+    static let pretendardRegular47: Font = .custom("Pretendard-Regular", size: 47)
+    static let pretendardRegular48: Font = .custom("Pretendard-Regular", size: 48)
+    static let pretendardRegular49: Font = .custom("Pretendard-Regular", size: 49)
+    static let pretendardRegular50: Font = .custom("Pretendard-Regular", size: 50)
+    
+    enum Pretendard {
+        case semibold
+        case medium
+        case bold
+        case custom(String)
+        
+        var value: String {
+            switch self {
+            case .semibold:
+                return "Pretendard-SemiBold"
+            case .medium:
+                return "Pretendard-Medium"
+            case .bold:
+                return "Pretendard-Bold"
+            case .custom(let name):
+                return name
+            }
+        }
+    }
+    
+    static func pre(_ type: Pretendard, size: CGFloat = 17) -> Font {
+        return custom(type.value , size: size)
+    }
+    
+}
